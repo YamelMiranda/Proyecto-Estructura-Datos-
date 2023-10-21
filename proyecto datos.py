4f6428e2367773044f08e8834e491e408357c0b3
YamelMiranda <yamelrmiranda@gmail.com>
2023-10-20 20:53:13 -0400

    commit

--- proyecto datos.py
+++ proyecto datos.py
@@ -1,6 +1,3 @@
-# TARJETAS DE CRÉDITO
-# DATOS DE LA TARJETA (Unidimensionales)
-
 recoleccion_datos = []
 
 clients = ["Charlie Gregorio", "Juan Gonzalo", "Idalberto Chiavenato", "Amanda Perez"]
@@ -34,7 +31,9 @@ while True:
     6) Matriz Simétrica
     7) Matriz Antisimétrica
     8) Matriz Binaria 
-    9) Salir
+    9) Colas
+    10) Pilas
+    11) Salir
     
     """))
 
@@ -105,8 +104,7 @@ while True:
             for j, amount in enumerate(row):
                 if amount > 0:
                     print(f"Mes {i + 1}, Mes {j + 1}: ${amount} pagado")
-
-        # Imprimir la matriz con barras verticales
+     
         print()
         print("Historial de Pagos de las Tarjetas en forma de matriz:")
         print("Leyenda \nFila: # Tarjeta \nColumna: Mes")
@@ -114,7 +112,6 @@ while True:
             for elemento in fila:
                 print(f"| {elemento:.2f} ", end="")
             print("|")  
-    
     
     elif opcion == 6:
         transacciones = [
@@ -138,7 +135,7 @@ while True:
         print("Matriz Simétrica:")
         print("Matriz de Transacciones Agosto-Sep-Oct:")
         for fila in matriz_simetrica:
-                print(fila)
+            print(fila)
         print("Leyenda \nFila: # Tarjeta \nColumna: Mes ")
         print()
 
@@ -164,20 +161,157 @@ while True:
         print("Matriz Antisimétrica:")
         print("Matriz de Saldo Disponible Agosto-Sep-Oct:")
         for fila in matriz_antisimetrica:
-         print(fila)
+            print(fila)
         print("Leyenda \nFila: # Tarjeta \nColumna: Mes ")
         print()
 
     elif opcion == 8:
-        matriz_binaria = [[int(activa) for activa in recoleccion_datos[2]]] * 3  # Replicamos la fila 3 veces
-        print("Matriz Binaria de Activación de Tarjetas:")
+        matriz_binaria = [
+        [1, 0, 1],
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1] ]
+ 
+        print("Matriz Binaria de Pago de Tarjetas por mes:")
+        print ("Mes de Septiembre\n")
         for fila in matriz_binaria:
-         print(fila)
+            print(' '.join(map(str, fila)))
         print()
 
     elif opcion == 9:
+        cola_tarjetas = []
+
+        def add(cliente, num_id, num_tarjeta):
+            nueva_tarjeta = {
+                "cliente": cliente,
+                "num_id": num_id,
+                "num_tarjeta": num_tarjeta
+            }
+            cola_tarjetas.append(nueva_tarjeta)
+            print(f"Se ha agregado la tarjeta de crédito de {cliente} a la cola.")
+
+        def remove():
+            if not cola_tarjetas:
+                print("La cola de tarjetas de crédito está vacía.")
+            else:
+                tarjeta = cola_tarjetas.pop(0)
+                print(f"Se ha retirado la tarjeta de crédito de {tarjeta['cliente']} de la cola.")
+                return tarjeta
+
+        def buscar_por_id(num_id):
+            for tarjeta in cola_tarjetas:
+                if tarjeta["num_id"] == num_id:
+                    print(f"Tarjeta encontrada - Cliente: {tarjeta['cliente']}, Número de Identificación: {tarjeta['num_id']}")
+                    return tarjeta
+            print(f"No se encontró ninguna tarjeta con el número de identificación {num_id}.")
+            return None
+
+        def mostrar_menu():
+            print("\nMenú de opciones:")
+            print("1) Agregar tarjeta de crédito (Add)")
+            print("2) Eliminar tarjeta de crédito(Remove)")
+            print("3) Buscar tarjeta de crédito por número de identificación(peek)")
+            print("4) Salir")
+
+        while True:
+            mostrar_menu()
+            opcion = input("Seleccione una opción: ")
+
+            if opcion == "1":
+                cliente = input("Ingrese el nombre del cliente: ")
+                num_id = input("Ingrese el número de identificación: ")
+                num_tarjeta = input("Ingrese el número de tarjeta de crédito: ")
+                add(cliente, num_id, num_tarjeta)
+            elif opcion == "2":
+                removed_card = remove()
+            elif opcion == "3":
+                num_id = input("Ingrese el número de identificación que desea buscar: ")
+                buscar_por_id(num_id)
+            elif opcion == "4":
+                print("Saliendo del programa. ¡Hasta luego!")
+                break
+
+    elif opcion == 10:
+        pila_clientes = []
+        pila_num_id = []
+        pila_num_tarjeta = []
+
+        def push_cliente(cliente):
+            pila_clientes.append(cliente)
+            print(f"Se ha agregado a la pila de clientes a {cliente}.")
+
+        def push_num_id(num_id):
+            pila_num_id.append(num_id)
+            print(f"Se ha agregado a la pila de números de identificación el valor {num_id}.")
+
+        def push_num_tarjeta(num_tarjeta):
+            pila_num_tarjeta.append(num_tarjeta)
+            print(f"Se ha agregado a la pila de números de tarjeta de crédito el valor {num_tarjeta}.")
+
+        def pop_cliente():
+            if not pila_clientes:
+                print("La pila de clientes está vacía.")
+            else:
+                cliente = pila_clientes.pop()
+                print(f"Se ha obtenido y eliminado de la pila de clientes a {cliente}.")
+                return cliente
+
+        def pop_num_id():
+            if not pila_num_id:
+                print("La pila de números de identificación está vacía.")
+            else:
+                num_id = pila_num_id.pop()
+                print(f"Se ha obtenido y eliminado de la pila de números de identificación el valor {num_id}.")
+                return num_id
+
+        def pop_num_tarjeta():
+            if not pila_num_tarjeta:
+                print("La pila de números de tarjeta de crédito está vacía.")
+            else:
+                num_tarjeta = pila_num_tarjeta.pop()
+                print(f"Se ha obtenido y eliminado de la pila de números de tarjeta de crédito el valor {num_tarjeta}.")
+
+        def peek_num_id():
+            if not pila_num_id:
+                print("La pila de números de identificación está vacía.")
+            else:
+                num_id = pila_num_id[-1]
+                print(f"El siguiente valor de salida de la pila de números de identificación es {num_id}.")
+
+        def mostrar_menu():
+            print("\nMenú de opciones:")
+            print("1. Agregar tarjeta de crédito")
+            print("2. Eliminar tarjeta de crédito")
+            print("3. Buscar tarjeta de crédito")
+            print("4. Salir")
+
+        while True:
+            mostrar_menu()
+            opcion = input("Seleccione una opción (1/2/3/4): ")
+
+            if opcion == "1":
+                cliente = input("Ingrese el nombre del cliente: ")
+                num_id = input("Ingrese el número de identificación: ")
+                num_tarjeta = input("Ingrese el número de tarjeta de crédito: ")
+                push_cliente(cliente)
+                push_num_id(num_id)
+                push_num_tarjeta(num_tarjeta)
+            elif opcion == "2":
+                pop_cliente()
+                pop_num_id()
+                pop_num_tarjeta()
+            elif opcion == "3":
+                opcion_busqueda = input("Buscar número de Identificación: ")
+                if opcion_busqueda == "1":
+                    num_id = input("Ingrese el número de identificación que desea buscar: ")
+                    if num_id in pila_num_id:
+                        print(f"Tarjeta encontrada - Número de Identificación: {num_id}")
+                    else:
+                        print(f"No se encontró ninguna tarjeta con el número de identificación {num_id}.")
+            elif opcion == "4":
+             print("Saliendo del programa. ¡Hasta luego!")
+            
+
+    elif opcion == 11:
         print("Saliendo...")
         break
-
-    else:
-        print("Opción no válida. Introduzca una opción válida.")
